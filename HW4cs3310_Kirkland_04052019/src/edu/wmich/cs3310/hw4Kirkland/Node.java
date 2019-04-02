56f619fdd23cba52d17c4192379829f419caac19
Michael <Michael@LAPTOP-FNO4A9K3>
2019-04-02 09:55:45 -0400

    Created Huffman Tree

--- HW4cs3310_Kirkland_04052019/src/edu/wmich/cs3310/hw4Kirkland/Node.java
+++ HW4cs3310_Kirkland_04052019/src/edu/wmich/cs3310/hw4Kirkland/Node.java
@@ -6,6 +6,45 @@ public class Node {
 	Map.Entry<Character, Integer> value;
 	Node nextN;
 	
+	
+	
+	//For Huffman Tree
+	int val;
+	char letter;
+	Node lChild;
+	Node rChild;
+	Node parent;
+	
+	public Node(int dataVal, Node left, Node right) {
+		val = dataVal;
+		lChild = left;
+		rChild = right;
+	}
+	
+	public Node(char letterVal) {
+		letter = letterVal;
+		lChild = null;
+		rChild = null;
+	}
+	
+	public int getVal() {
+		return val;
+	}
+	
+	public Node leftChild() {
+		return lChild;
+	}
+	
+	public Node rightChild() {
+		return rChild;
+	}
+	
+	
+	//End of Huffman Tree
+	
+	
+	
+	
 	public Node(Map.Entry<Character, Integer> dataValue){// Constructor
 		value = dataValue;
 		nextN = null;
